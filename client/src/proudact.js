dc18d2cbc151c742562523642c01aada20779a50
Aryeh Packer <arieh87@gmail.com>
2022-02-06 20:40:02 +0200

    first shop commit added client and server

---
+++ client/src/proudact.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function proudact() {
+  return <div></div>;
+}
+
+export default proudact;
---
+++ server/server.mjs
@@ -0,0 +1,25 @@
+import http from 'http';
+import fetch from "node-fetch";
+import URLParse from "url-parse";
+
+const hostname = '127.0.0.1';
+const port = '3001';
+
+const server = http.createServer(async(req, res) =>{
+    res.statusCode = 200;
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+    res.setHeader('Access-Control-Max-Age', 2592000);
+    res.setHeader('Content-Type', 'application/json');
+
+    const response = await fetch('https://fakestoreapi.com/products');
+    const data =  await response.json();
+
+    res.end(JSON.stringify(data))
+
+})
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+  });
+
