e5f5b803fe553bb9395dd7162b8f17c09248c892
Aryeh Packer <arieh87@gmail.com>
2022-02-08 15:26:53 +0200

    added components

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,25 @@
 import './App.css';
+import Proudact from './Proudact';
+import { useEffect , useState} from 'react';
 
 function App() {
+
+  const [proudacts, setProudacts] = useState([]);
+
+   useEffect(() => {
+     fetch('http://localhost:3001')
+     .then(response => response.json())
+     .then(data => setProudacts(data));
+   }, []);
+
   return (
     <div className="App">
+      {proudacts.map(product => {
+          return ( 
+          <Proudact {...product}/>
+          );
+      })}
+
       
     </div>
   );
--- client/src/proudact.js
+++ client/src/proudact.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
-function proudact() {
-  return <div></div>;
+function Proudact({id,title,image,price,description,category}) {
+  return (
+    <div className='proudact'>
+
+      <div className='imageContainer'>
+          <img className='image' src={image} />
+        </div>
+        <label className='category'>{category}</label>
+        <h3>{title}</h3>
+        <p>{description}</p>
+        <button>{price}</button>
+
+  </div>);
 }
 
-export default proudact;
+export default Proudact;
